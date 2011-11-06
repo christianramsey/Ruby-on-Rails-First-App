5e101d7db0dec1a3b1c5514376fdf2542e15a21c
Christian Ramsey <thechristianramsey@Christians-MacBook-Pro.local>
2011-11-06 09:52:51 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRails::Application.config.secret_token = 'd84e190823aaf4a48999a070c9c02379dfde827fcd4ae86c968cd4560423448bf28afa48adf49091f0570cdeb595d523af09d8923781d9b821f3f2ec2ec40000'
